2ef3a872817d2a3e0d582d4b30708cbd339e2659
Eric Zheng <greenserpent2007@gmail.com>
2025-01-07 10:44:57 -0500

    BALLS

---
+++ Module 1.py
@@ -0,0 +1,9 @@
+var1 = 8
+var2 = 19
+var3 = 99
+var4 = "eight"
+var5 = "Baylor"
+
+print("Let's party at " + var5 + " like it's " + str(var2) + str(var3) + ".") 
+print("The sum of " + str(var1) + " and " + str(var2) + " is 27.")
+print((var4 * 3) + " could be written as " + (str(var1) * 3) + ".")
