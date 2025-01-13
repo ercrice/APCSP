4e413e506932f053324404d0be967da4883880d4
Eric Zheng <greenserpent2007@gmail.com>
2025-01-13 15:01:36 -0500

    0220

---
+++ Module 2a.py
@@ -0,0 +1,7 @@
+num1 = input("Enter Number 1: ")
+num2 = input("Enter Number 2: ")
+num3 = input("Enter Number 3: ")
+
+avg = (int(num1) + int(num2) + int(num3)) / 3
+
+print("The average of your three numbers " + num1 + ", " + num2 + ", and " + num3 + " is " + str(avg))
---
+++ Module 2b.py
@@ -0,0 +1,8 @@
+FirstName = input("Enter your first name: ")
+LastName = input("Enter your last name: ")
+BirthDay = input("Enter your birth day in numbers: ")
+BirthMonth = input("Enter your birth month in words: ") 
+BirthYear = input("Enter your birth year in numbers: ")
+Last4OfSocial = input("Enter last 4 digits of your social security: ")
+
+print(FirstName + " " + LastName + "\n" + BirthMonth + " " + BirthDay + ", " + BirthYear + "\n" + Last4OfSocial)
